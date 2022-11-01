d7450d9839779193ed1be7f4cdbccfcb4b2021f7
dmanh204 <115561854+dmanh204@users.noreply.github.com>
2022-11-01 20:15:32 +0700

    Update test_key_generate.py

--- test_key_generate.py
+++ test_key_generate.py
@@ -1,23 +1,5 @@
 from des import *
 
-def taokey(khoa, bokhoa):
-      hoanvibandau = hoanvi(khoa, permuted_choice_one)      # hoan vi ban dau: dung
-      
-      left = [0,0,0,0]
-      right = [0,0,0,0]
-      giatrighep = [0,0,0,0,0,0,0]
-      tach56(hoanvibandau, left, right)
-      
-      for i in range(16):
-            dichvong(left, schedule_of_left_shift[i])
-            dichvong(right, schedule_of_left_shift[i])
-            trai = left.copy()
-            phai = right.copy()
-            khoacon = key(trai, phai)
-            bokhoa.append(khoacon)
-            ghep56(giatrighep, trai, phai)
-            bokhoa[i].keyround = hoanvi(giatrighep, permuted_choice_two)
-
 khoa = [0x0f, 0x15, 0x71, 0xc9, 0x47, 0xd9, 0xe8, 0x59]
 bokhoa = []
 taokey(khoa, bokhoa)
